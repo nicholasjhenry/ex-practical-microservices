01018736e1700ed593995b6c93cb8f8491c9f0f2
Nicholas Henry <nicholas.henry@gmail.com>
2020-12-27 09:19:32 -0500

    Add mix watch and own mix lock file

--- packages/message_store/mix.exs
+++ packages/message_store/mix.exs
@@ -8,7 +8,7 @@ defmodule MessageStore.MixProject do
       build_path: "../../_build",
       config_path: "./config/config.exs",
       deps_path: "../../deps",
-      lockfile: "../../mix.lock",
+      lockfile: "mix.lock",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -30,6 +30,7 @@ defmodule MessageStore.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:postgrex, "~> 0.15.7"},
       {:jason, "~> 1.0"},
       {:elixir_uuid, "~> 1.2"}
