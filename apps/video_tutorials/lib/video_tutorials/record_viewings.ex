11f06ce3e0f6704fff3baa42dd3f841379fb88b9
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2020-12-27 13:19:53 -0500

    Write a video viewed event (#5)

---
+++ apps/video_tutorials/lib/video_tutorials/record_viewings.ex
@@ -0,0 +1,23 @@
+defmodule VideoTutorials.RecordViewings do
+  alias MessageStore.NewMessage
+
+  def record_viewing(trace_id, video_id, user_id) do
+    stream_name = "viewing-#{video_id}"
+
+    viewed_event = NewMessage.new(
+      stream_name: stream_name,
+      type: "VideoViewed",
+      metadata: %{
+        trace_id: trace_id,
+        user_id: user_id
+      },
+      data: %{
+        user_id: user_id,
+        video_id: video_id
+      }
+    )
+
+    # message_store.write(stream_name, viewed_event) ??
+    MessageStore.write_message(viewed_event)
+  end
+end
