8bd786ec834a657b5ccdadfa9eb3e474cf59ed69
Nicholas Henry <nicholas.henry@gmail.com>
2021-02-18 11:06:09 -0500

    Adding Validation to a Component

---
+++ apps/video_tutorials/lib/video_tutorials/components/name_video.ex
@@ -0,0 +1,107 @@
+defmodule VideoTutorials.NameVideo do
+  alias MessageStore.NewMessage
+  alias VideoTutorials.VideoPublishing.VideoPublishingProjection
+
+  def handle_message(%{type: "NameVideo"} = command) do
+    context = %{video_id: command.data["video_id"], command: command}
+
+    with context = load_video(context),
+          {:ok, context} <- ensure_command_has_not_been_processed(context),
+          {:ok, context} <- ensure_name_is_valid(context),
+          _context = write_video_named_event(context) do
+            :ok
+    else
+      {:error, {:command_already_processed, _context}} -> :ok
+      {:error, {:validation_error, changeset, context}} -> write_video_name_rejected(context, changeset.errors)
+    end
+  end
+
+  defp load_video(context) do
+    video_stream_name = "videoPublishing-#{context.video_id}"
+
+    maybe_video = MessageStore.fetch(video_stream_name, VideoPublishingProjection)
+
+    Map.put(context, :video, maybe_video)
+  end
+
+  defp ensure_command_has_not_been_processed(context) do
+    command = context.command
+    video = context.video
+
+    if video.sequence > command.global_position do
+      {:error, {:command_already_processed, context}}
+    else
+      {:ok, context}
+    end
+  end
+
+  defp ensure_name_is_valid(context) do
+    import Ecto.Changeset
+
+    command = context.command
+    types = %{name: :string}
+
+    {context.video, types}
+    |> cast(%{name: command.data["name"]}, Map.keys(types))
+    |> validate_required(~w/name/a)
+    |> apply_action(:insert)
+    |> case do
+      {:ok, _valid_input} -> {:ok, context}
+      {:error, changeset} -> {:error, {:validation_error, changeset, context}}
+    end
+  end
+
+  defp write_video_named_event(context) do
+    command = context.command
+
+    stream_name = "videoPublishing-#{command.data["video_id"]}"
+
+    video_named_event = NewMessage.new(
+      stream_name: stream_name,
+      type: "videoNamed",
+      metadata: %{
+        trace_id: Map.fetch!(command.metadata, "trace_id"),
+        user_id: Map.fetch!(command.metadata, "user_id"),
+      },
+      data: %{
+        name: Map.fetch!(command.data, "name")
+      },
+      # TODO
+      expected_version: nil
+    )
+
+    MessageStore.write_message(video_named_event)
+
+    context
+  end
+
+  defp write_video_name_rejected(context, errors) do
+    command = context.command
+
+    stream_name = "videoNamed-#{command.data["video_id"]}"
+
+    reason =
+      errors
+      |> Enum.map(fn {field, {msg, _metadata}} -> {field, msg} end)
+      |> Map.new
+
+    video_name_rejected_event = NewMessage.new(
+      stream_name: stream_name,
+      type: "videoNameRejected",
+      metadata: %{
+        trace_id: Map.fetch!(command.metadata, "trace_id"),
+        user_id: Map.fetch!(command.metadata, "user_id"),
+      },
+      data: %{
+        name: Map.fetch!(command.data, "name"),
+        reason: reason
+      },
+      # TODO
+      expected_version: nil
+    )
+
+    MessageStore.write_message(video_name_rejected_event)
+
+    context
+  end
+end
--- apps/video_tutorials/lib/video_tutorials/components/video_publishing/video_publishing_projection.ex
+++ apps/video_tutorials/lib/video_tutorials/components/video_publishing/video_publishing_projection.ex
@@ -1,15 +1,23 @@
 defmodule VideoTutorials.VideoPublishing.VideoPublishingProjection do
-  defstruct [:id, :publishing_attempted?, :source_uri, :transcoded_uri, :owner_id]
+  defstruct [:id, :publishing_attempted?, :source_uri, :transcoded_uri, :owner_id, :sequence, :name]
 
   def init() do
-    %__MODULE__{publishing_attempted?: false}
+    %__MODULE__{publishing_attempted?: false, sequence: 0, name: ""}
   end
 
-  def apply(video, %{type: "videoPublished", data: data}) do
-    %{video | id: data["video_id"], publishing_attempted?: true, owner_id: data["owner_id"], source_uri: data["source_uri"], transcoded_uri: data["transcoded_uri"]}
+  def apply(video, %{type: "videoPublished", data: data, global_position: global_position}) do
+    %{video | id: data["video_id"], publishing_attempted?: true, owner_id: data["owner_id"], source_uri: data["source_uri"], transcoded_uri: data["transcoded_uri"], sequence: global_position}
   end
 
-  def apply(video, %{type: "videoFailed", data: data}) do
-    %{video | id: data["video_id"], publishing_attempted?: true, owner_id: data["owner_id"], source_uri: data["source_uri"], transcoded_uri: data["transcoded_uri"]}
+  def apply(video, %{type: "videoFailed", data: data, global_position: global_position}) do
+    %{video | id: data["video_id"], publishing_attempted?: true, owner_id: data["owner_id"], source_uri: data["source_uri"], transcoded_uri: data["transcoded_uri"], sequence: global_position}
+  end
+
+  def apply(video, %{type: "videoNamed", data: data, global_position: global_position}) do
+    %{video | name: data["name"], sequence: global_position}
+  end
+
+  def apply(video, %{type: "videoNameRejected", data: data, global_position: global_position}) do
+    %{video | name: data["name"], sequence: global_position}
   end
 end
