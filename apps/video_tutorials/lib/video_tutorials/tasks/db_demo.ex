169084c7e4072f729cbff64caa5d81584a7d3191
Nicholas Henry <nicholas.henry@gmail.com>
2021-02-23 09:31:57 -0500

    Add demo script

---
+++ apps/video_tutorials/lib/video_tutorials/tasks/db_demo.ex
@@ -0,0 +1,21 @@
+defmodule VideoTutorials.Tasks.DbDemo do
+  import VideoTutorials.Tasks
+
+  def exec do
+    seed(:video_tutorials, [VideoTutorials.Repo])
+  end
+
+  def seed(app, repos) do
+    start_services(app, repos)
+
+    run_seeds(app)
+
+    stop_services()
+  end
+
+  def run_seeds(app) do
+    IO.puts("Running demo script..")
+    seed_script = Path.join([:code.priv_dir(app), "repo/demo.exs"])
+    Code.eval_file(seed_script)
+  end
+end
