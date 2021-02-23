ae27adc5c96c51012fe0278ffbb7f61692ebba82
Nicholas Henry <nicholas.henry@gmail.com>
2021-02-23 09:24:43 -0500

    Add a handler for publishing videos

--- apps/creators_portal/lib/creators_portal/aggregators/creators_videos.ex
+++ apps/creators_portal/lib/creators_portal/aggregators/creators_videos.ex
@@ -4,6 +4,27 @@ defmodule CreatorsPortal.CreatorsVideos do
 
   import Ecto.Query
 
+  def handle_message(%{type: "videoPublished"} = event) do
+    video_id = stream_to_entity_id(event.stream_name)
+    position = event.position
+    owner_id = Map.fetch!(event.data, "owner_id")
+    source_uri = Map.fetch!(event.data, "source_uri")
+    transcoded_uri = Map.fetch!(event.data, "transcoded_uri")
+
+    video = %Video{
+      id: video_id,
+      owner_id: owner_id,
+      name: "Untitled",
+      description: "No description",
+      views: 0,
+      source_uri: source_uri,
+      transcoded_uri: transcoded_uri,
+      position: position
+    }
+
+    Repo.insert!(video, on_conflict: :nothing)
+  end
+
   def handle_message(%{type: "VideoNamed"} = event) do
     video_id = stream_to_entity_id(event.stream_name)
     position = event.position
