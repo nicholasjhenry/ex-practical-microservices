e6ce89c1a51841ca92f847ae409fb8c811ca6382
Nicholas Henry <nicholas.henry@gmail.com>
2021-02-24 08:38:11 -0500

    Remove redundant worker
    Was move to back-end application

--- apps/creators_portal/lib/creators_portal/application.ex
+++ apps/creators_portal/lib/creators_portal/application.ex
@@ -5,8 +5,6 @@ defmodule CreatorsPortal.Application do
 
   use Application
 
-  @mix_env Mix.env()
-
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
@@ -14,19 +12,8 @@ defmodule CreatorsPortal.Application do
       {Phoenix.PubSub, name: CreatorsPortal.PubSub}
       # Start a worker by calling: CreatorsPortal.Worker.start_link(arg)
       # {CreatorsPortal.Worker, arg}
-    ] ++ consumers(@mix_env)
+    ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: CreatorsPortal.Supervisor)
-  end
-
-   # TODO: add own supervisor for consumers
-  defp consumers(:test), do: []
-  defp consumers(_env) do
-    [
-      {
-        MessageStore.SubscriberWorker,
-        [config: %{stream_name: "aggregators:creators-videos", subscribed_to: "videoPublishing", handler: VideoTutorials.CreatorsVideos}]
-      }
-    ]
   end
 end
