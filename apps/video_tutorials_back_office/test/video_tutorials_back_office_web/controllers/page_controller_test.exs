ae1ed11362fade949b5f45f7745335def19c1608
Nicholas Henry <nicholas.henry@gmail.com>
2021-02-26 10:06:01 -0500

    Update tests

--- apps/video_tutorials_back_office/test/video_tutorials_back_office_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule VideoTutorialsBackOfficeWeb.PageControllerTest do
-  use VideoTutorialsBackOfficeWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/admin")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
--- apps/video_tutorials_back_office/test/video_tutorials_back_office_web/live/page_live_test.exs
+++ apps/video_tutorials_back_office/test/video_tutorials_back_office_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule VideoTutorialsBackOfficeWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/admin")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Admin"
+    assert render(page_live) =~ "Admin"
   end
 end
