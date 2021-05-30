130d690d97cb94314a972755b20adc8397055dbc
Nicholas Henry <nicholas.henry@gmail.com>
2021-05-30 18:24:54 -0400

    Add context functions for listing messages

--- apps/video_tutorials_back_office/lib/video_tutorials_back_office.ex
+++ apps/video_tutorials_back_office/lib/video_tutorials_back_office.ex
@@ -18,6 +18,23 @@ defmodule VideoTutorialsBackOffice do
     Repo.all(query)
   end
 
+  def list_messages(category: category, user_id: user_id) do
+    query = from(messages in Message,
+      where: fragment("category(?)", messages.stream_name) == type(^category, :string),
+      where: fragment("data->>'user_id'") == type(^user_id, :string),
+      order_by: messages.global_position
+      )
+    Repo.all(query)
+  end
+
+  def list_messages(stream_name: stream_name) do
+    query = from(messages in Message,
+      where: [stream_name: ^stream_name],
+      order_by: messages.global_position
+      )
+    Repo.all(query)
+  end
+
   def list_messages(%{type: type}) do
     query = from(messages in Message, order_by: messages.global_position, where: messages.type == ^type)
     Repo.all(query)
