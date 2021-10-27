c93be8d280de989c561311e93bdc010d1982fda7
Nicholas Henry <nicholas.henry@gmail.com>
2021-10-27 09:51:52 -0400

    Unsure why there are still being formatted

--- apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/consumers/commands.ex
+++ apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/consumers/commands.ex
@@ -2,5 +2,5 @@ defmodule VideoTutorialsServices.IdentityComponent.Consumers.Commands do
   use Verity.Consumer.Postgres
 
   alias VideoTutorialsServices.IdentityComponent.Handlers
-  handler Handlers.Commands
+  handler(Handlers.Commands)
 end
