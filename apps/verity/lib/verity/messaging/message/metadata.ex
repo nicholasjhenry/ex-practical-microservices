344b3b8eca5094edd8b9719b20cd01a50e915301
Nicholas Henry <nicholas.henry@gmail.com>
2021-10-28 16:41:29 -0400

    Encapsulate video naming messages

--- apps/verity/lib/verity/messaging/message/metadata.ex
+++ apps/verity/lib/verity/messaging/message/metadata.ex
@@ -3,13 +3,22 @@ defmodule Messaging.Message.Metadata do
   http://docs.eventide-project.org/user-guide/messages-and-message-data/metadata.html#messaging-message-metadata-class
   """
 
-  defstruct [:user_id, :trace_id, :origin_stream_name]
+  defstruct [:user_id, :trace_id, :origin_stream_name, :global_position]
 
   def parse(raw) do
     %__MODULE__{
       user_id: raw["userId"],
       trace_id: raw["traceId"],
       origin_stream_name: raw["originStreamName"]
+    }
+  end
+
+  def from_message_data(message_data) do
+    %__MODULE__{
+      user_id: message_data.metadata["userId"],
+      trace_id: message_data.metadata["traceId"],
+      origin_stream_name: message_data.metadata["originStreamName"],
+      global_position: message_data.global_position
     }
   end
 
