4d95f27a4a24ec0bde33b80779ebe6496d6b57f7
Nicholas Henry <nicholas.henry@gmail.com>
2021-10-28 14:50:22 -0400

    Encapsulate registration email sent event

--- apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/messages/events/registration_email_sent.ex
+++ apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/messages/events/registration_email_sent.ex
@@ -1,17 +1,37 @@
 defmodule VideoTutorialsServices.IdentityComponent.Messages.Events.RegistrationEmailSent do
-  alias MessageStore.MessageData
+  use Verity.Messaging.Message
 
-  def follow(message) do
+  defstruct [:metadata, :user_id, :email_id]
+
+  def build(metadata, attrs) do
+    struct(__MODULE__, attrs) |> Map.put(:metadata, metadata)
+  end
+
+  def follow(message, attrs \\ %{}, meta \\ %{}) do
+    fields = message |> Map.from_struct() |> Map.merge(attrs)
+    new_message = struct!(__MODULE__, fields)
+    %{new_message | metadata: Map.merge(message.metadata, meta)}
+  end
+
+  def parse(message_data) do
+    %__MODULE__{
+      metadata: Metadata.parse(message_data.metadata),
+      user_id: Map.fetch!(message_data.data, "userId"),
+      email_id: Map.fetch!(message_data.data, "emailId")
+    }
+  end
+
+  def to_message_data(message) do
     MessageData.Write.new(
       stream_name: nil,
       type: "RegistrationEmailSent",
       metadata: %{
-        "traceId" => Map.fetch!(message.metadata, "traceId"),
-        "userId" => nil
+        "traceId" => message.metadata.trace_id,
+        "userId" => message.metadata.user_id
       },
       data: %{
-        "emailId" => Map.fetch!(message.data, "emailId"),
-        "userId" => nil
+        "emailId" => message.email_id,
+        "userId" => message.user_id
       },
       # TODO
       expected_version: nil
