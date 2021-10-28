9d0cc940ac5369a0f910cccaedccb021da804581
Nicholas Henry <nicholas.henry@gmail.com>
2021-10-28 14:09:10 -0400

    Encapsulate register and registered messages

--- apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/handlers/commands.ex
+++ apps/video_tutorials_services/lib/video_tutorials_services/components/identity_component/handlers/commands.ex
@@ -1,28 +1,29 @@
 defmodule VideoTutorialsServices.IdentityComponent.Handlers.Commands do
   use Verity.Messaging.Handler
 
+  alias VideoTutorialsServices.IdentityComponent.Messages.Commands.Register
   alias VideoTutorialsServices.IdentityComponent.Messages.Events.Registered
   alias VideoTutorialsServices.IdentityComponent.Store
-
-  @category :identity
 
   defmodule AlreadyRegisteredError do
     defexception [:message]
   end
 
   @impl true
-  def handle_message(%{type: "Register"} = register), do: register_user(register)
+  def handle_message(%{type: "Register"} = message_data) do
+    message_data |> Register.parse() |> register_user()
+  end
+
   def handle_message(_), do: nil
 
   defp register_user(register) do
-    identity_id = register.data["userId"]
-    identity = Store.fetch(identity_id)
+    identity = Store.fetch(register.user_id)
 
     if identity.registered? do
       raise AlreadyRegisteredError
     end
 
-    stream_name = stream_name(register.data["userId"], @category)
+    stream_name = stream_name(register.user_id, :identity)
     registered = Registered.follow(register)
     write(registered, stream_name)
   end
